bff36006e0f87231825b180b700ff7dbcc69ea6e
Xin Huang <patz.hwang@gmail.com>
2014-04-28 00:08:26 +0800

    pieces should have boarder

--- 2048/Presenter/Scene.cpp
+++ 2048/Presenter/Scene.cpp
@@ -10,23 +10,28 @@ using namespace ci::app;
 using namespace std;
 
 struct Scene::Data {
-  float borderSize = 0.f;
+  float borderSize = 2.f;
 };
 
 Scene::Scene() : d(make_unique<Data>()) {}
 Scene::~Scene() {}
 
 ci::Vec2f Scene::cellSize() {
-  return { boardWidth() / Config::SIZE, boardHeight() / Config::SIZE };
+  auto borderWidthTotal = (Config::SIZE + 1) * data()->borderSize;
+  auto boardHeightTotal = (Config::SIZE + 1) * data()->borderSize;
+  return {(boardWidth() - borderWidthTotal) / Config::SIZE,
+          (boardHeight() - boardHeightTotal) / Config::SIZE };
+}
+
+ci::Vec2f Scene::cellPos(const ci::Vec2i &coord) {
+  return boardPos() + Vec2f(data()->borderSize, data()->borderSize) +
+         static_cast<Vec2f>(coord) *
+             (cellSize() + Vec2f(data()->borderSize, data()->borderSize));
 }
 
 ci::Vec2f Scene::distance(const ci::Vec2i &from, const ci::Vec2i &to) {
   return Vec2f(from - to) *
          (cellSize() + Vec2f(data()->borderSize, data()->borderSize));
-}
-
-ci::Vec2f Scene::cellPos(const ci::Vec2i &coord) {
-  return boardPos() + static_cast<Vec2f>(coord) * cellSize();
 }
 
 float Scene::boardWidth() {
